0e91104601d87868a374ad4c10ac6102f0ff898d
Dr. Bleep <drbleep@bleeplabs.com>
2016-12-29 15:39:47 -0600

    Update RP01 -001.ino

--- RP01 -001.ino
+++ RP01 -001.ino
@@ -2,14 +2,6 @@
 The Rad-Fi Patchable Synthesizer RS01
 http://bleeplabs.com/store/the-rad-fi-system/ 
 
-This works well but needs some work by the time the Rad-Fis ship
-
-
-Todo:
-commenting and moding instructions
-PWM rate
-general loop speedup
-
 */
 #include <avr/pgmspace.h>
 //#define DIGITALIO_NO_MIX_ANALOGWRITE
